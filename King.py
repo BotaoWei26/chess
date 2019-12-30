773540a52d664ff153096f8e0470405249c891d3
Bo-PC <b26wei@uwaterloo.ca>
2019-12-30 01:17:09 -0500

    fixed issue of castling through an attack

--- King.py
+++ King.py
@@ -35,23 +35,33 @@ class King(Piece):
                 if [piece.row, piece.col] == [0, 0] and piece.first_move and \
                     not detect_collision([0, 1], pieces) and not detect_collision([0, 2], pieces) and \
                     not detect_collision([0, 3], pieces) and \
-                    not check_check_all(pieces, self.color, [], self):
+                    not check_check_all(pieces, self.color, [], self) and \
+                    not check_check_all(pieces, self.color, [0, 1], self) and \
+                    not check_check_all(pieces, self.color, [0, 2], self) and \
+                    not check_check_all(pieces, self.color, [0, 3], self):
                     moves.append(["castling", [0, 2], [0, 3], [piece.row, piece.col]])
                 elif [piece.row, piece.col] == [0, 7] and piece.first_move and \
                     not detect_collision([0, 5], pieces) and not detect_collision([0, 6], pieces) and \
-                    not check_check_all(pieces, self.color, [], self):
+                    not check_check_all(pieces, self.color, [], self) and \
+                    not check_check_all(pieces, self.color, [0, 5], self) and \
+                    not check_check_all(pieces, self.color, [0, 6], self):
                     moves.append(["castling", [0, 6], [0, 5], [piece.row, piece.col]])
 
         if self.color == 'w' and [self.row, self.col] == [7, 4] and self.first_move:
             for piece in pieces:
                 if [piece.row, piece.col] == [7, 0] and piece.first_move and \
                     not detect_collision([7, 1], pieces) and not detect_collision([7, 2], pieces) and \
-                    not detect_collision([7, 3], pieces)and \
-                    not check_check_all(pieces, self.color, [], self):
+                    not detect_collision([7, 3], pieces) and \
+                    not check_check_all(pieces, self.color, [], self) and \
+                    not check_check_all(pieces, self.color, [7, 1], self) and \
+                    not check_check_all(pieces, self.color, [7, 2], self) and \
+                    not check_check_all(pieces, self.color, [7, 3], self):
                     moves.append(["castling", [7, 2], [7, 3], [piece.row, piece.col]])
                 elif [piece.row, piece.col] == [7, 7] and piece.first_move and \
                     not detect_collision([7, 5], pieces) and not detect_collision([7, 6], pieces) and \
-                    not check_check_all(pieces, self.color, [], self):
+                    not check_check_all(pieces, self.color, [], self) and \
+                    not check_check_all(pieces, self.color, [7, 5], self) and \
+                    not check_check_all(pieces, self.color, [7, 6], self):
                     moves.append(["castling", [7, 6], [7, 5], [piece.row, piece.col]])
         if real:
             moves = list(filter(lambda x: not check_check_all(pieces, self.color, x[1], self), moves))
